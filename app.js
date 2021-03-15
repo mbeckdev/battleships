b26a89c752a1f7963f188939cdf124b34709135d
mbeckdev <mbeckdev42@gmail.com>
2021-03-15 12:30:00 -0600

    Begin adding generated ships to computer grid

--- app.js
+++ app.js
@@ -32,4 +32,65 @@ document.addEventListener('DOMContentLoaded', () => {
 
   createBoard(userGrid, userSquares);
   createBoard(computerGrid, computerSquares);
+
+  //Ships
+  const shipArray = [
+    {
+      name: 'destroyer',
+      directions: [
+        [0, 1],
+        [0, width],
+      ],
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: 'battleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width * 2, width * 3],
+      ],
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width * 2, width * 3, width * 4],
+      ],
+    },
+  ];
+
+  // Draw the computer's ships in random locations
+  function generate(ship) {
+    let randomDirection = Math.floor(Math.random() * ship.directions.length);
+    let current = ship.directions[randomDirection];
+    if (randomDirection === 0) direction = 1;
+    if (randomDirection === 1) direction = 10;
+    let randomStart = Math.floor(
+      Math.random() * computerSquares.length -
+        ship.directions[0].length * direction
+    );
+
+    const isTaken = current.some((index) =>
+      computerSquares[randomStart + index].classList.contains('taken')
+    );
+    const isAtRightEdge = current.some(
+      (index) => (randomStart + index) % width === width - 1
+    );
+    const isAtLeftEdge = current.some(
+      (index) => (randomStart + index) % width === 0
+    );
+  }
 });
